41881824f35cb28e43ff6531d1b2ac8f05eedcf4
quiryat zavala <qzavala2014@gmail.com>
2024-01-26 15:02:43 -0800

    updated puppypals

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
- 
+  const [puppies, setPuppies] = useState(puppyList)
+  // console.log(puppies)
+  console.log("puppyList: ", puppyList);
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
      
     </>
   )
