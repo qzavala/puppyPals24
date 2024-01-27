da9750b9c3e8fbb622f3cb2305853901a2c8701d
quiryat zavala <qzavala2014@gmail.com>
2024-01-26 19:45:09 -0800

    added css elements

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,47 @@ import { useState } from 'react'
 import {puppyList} from './data.js'
 
 import './App.css'
+// import './index.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+
+  const [featPupId, setFeatPupId] = useState(null)
   // console.log(puppies)
   console.log("puppyList: ", puppyList);
 
-
+  function handleClick() {
+    // some logic here
+  }
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
   return (
+    
     <>
-      <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
-    </div>
+      <h1>Puppy pals</h1> 
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
      
-    </>
+    </div>
+  
+  </>
   )
 }
+
 
 export default App
